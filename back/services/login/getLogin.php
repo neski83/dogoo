cc7fd9a1103d1d882687b5fbd5a4afbfc3b02b52
laura.jorge <laura.jorge@globallogic.com>
2018-04-06 17:25:57 -0300

    add db

--- back/services/login/getLogin.php
+++ back/services/login/getLogin.php
@@ -1,43 +1,18 @@
-<?php
-	include("../../conexion/conexion.php");
-
-    header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
-    header("Pragma: no-cache"); // HTTP 1.0.
-    header("Expires: 0"); // Proxies.
-    
-$email=$_POST['email1']; // Fetching Values from URL.
-$password= sha1($_POST['password1']); // Password Encryption, If you like you can also leave sha1.
-// check if e-mail address syntax is valid or not
-$email = filter_var($email, FILTER_SANITIZE_EMAIL); // sanitizing email(Remove unexpected symbol like <,>,?,#,!, etc.)
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-echo "Invalid Email.......";
-}else{
-// Matching user input email and password with stored email and password in database.
-$result = mysql_query("SELECT * FROM registration WHERE email='$email' AND password='$password'");
-$data = mysql_num_rows($result);
-if($data==1){
-echo "Successfully Logged in...";
-}else{
-echo "Email or Password is wrong...!!!!";
-}
-}
-mysql_close ($connection); // Connection Closed.
-?>
-
 <?php 
 	include("../../conexion/conexion.php");
 
     header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
     header("Pragma: no-cache"); // HTTP 1.0.
     header("Expires: 0"); // Proxies.
-
+    $email=$_POST['email1']; // Fetching Values from URL.
+    $pass= $_POST['password']; 
     $conn = new Conexion();
-    $sql = "Select * from provincias order by provincia";
-    $provincias = $conn->consultar($sql);
+    $sql = "SELECT * FROM usuario WHERE email='$email' AND pass='$password'";
+    $data = $conn->consultar($sql);
+    if($data==1){
+    echo "Successfully Logged in...";
+    }else{
+    echo "Email or Password is wrong...!!!!";
+    }
     $conn->cerrarConexion();
-    foreach($provincias as &$l){
-        $l = array_map('utf8_encode', $l);
-    }    
-    echo json_encode($provincias);
-
-     ?>
+    ?>
--- main.js
+++ main.js
@@ -12,6 +12,7 @@ $(function() {
         cargarSelectDeRazas();    
         cargarSelectDeLocalidad();
         cargarSelectDeTransaccion();
+        function(data);
     }
 
     /* 
@@ -66,13 +67,11 @@ $(function() {
     $("#login").click(function(){
         var email = $("#email").val();
         var password = $("#password").val();
-        // Checking for blank fields.
         if( email =='' || password ==''){
         $('input[type="text"],input[type="password"]').css("border","2px solid red");
         $('input[type="text"],input[type="password"]').css("box-shadow","0 0 3px red");
         alert("Please fill all fields...!!!!!!");
         }else {
-        $.post("login.php",{ email1: email, password1:password},
         function(data) {
         if(data=='Invalid Email.......') {
         $('input[type="text"]').css({"border":"2px solid red","box-shadow":"0 0 3px red"});
