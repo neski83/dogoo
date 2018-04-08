b9305e0d53b8333653add4d4f9cce4e64cc67c45
lau <maria.laura.jorge1@gmail.com>
2018-04-08 05:21:12 -0300

    adding front

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ $(function() {
         cargarSelectDeRazas();    
         cargarSelectDeLocalidad();
         cargarSelectDeTransaccion();
-        function(data);
     }
 
     /* 
@@ -64,30 +63,4 @@ $(function() {
           });
         });
     });
-    $("#login").click(function(){
-        var email = $("#email").val();
-        var password = $("#password").val();
-        if( email =='' || password ==''){
-        $('input[type="text"],input[type="password"]').css("border","2px solid red");
-        $('input[type="text"],input[type="password"]').css("box-shadow","0 0 3px red");
-        alert("Please fill all fields...!!!!!!");
-        }else {
-        function(data) {
-        if(data=='Invalid Email.......') {
-        $('input[type="text"]').css({"border":"2px solid red","box-shadow":"0 0 3px red"});
-        $('input[type="password"]').css({"border":"2px solid #00F5FF","box-shadow":"0 0 5px #00F5FF"});
-        alert(data);
-        }else if(data=='Email or Password is wrong...!!!!'){
-        $('input[type="text"],input[type="password"]').css({"border":"2px solid red","box-shadow":"0 0 3px red"});
-        alert(data);
-        } else if(data=='Successfully Logged in...'){
-        $("form")[0].reset();
-        $('input[type="text"],input[type="password"]').css({"border":"2px solid #00F5FF","box-shadow":"0 0 5px #00F5FF"});
-        alert(data);
-        } else{
-        alert(data);
-        }
-        });
-        }
-        });
 });
